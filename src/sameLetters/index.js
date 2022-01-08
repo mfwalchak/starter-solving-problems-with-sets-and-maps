d1b34695428cd1e09553cfab06765cbbbf9932d5
Mike Walchak <mfwalchak@gmail.com>
2022-01-08 13:38:08 -0700

    solved for all except adding Map values

--- src/sameLetters/index.js
+++ src/sameLetters/index.js
@@ -2,6 +2,40 @@
  * return a Map of the unique letters and arrays of words that are made up of those letters
  * @param {array} words An array  of words
  */
-function sameLetters(words) {}
+function sameLetters(words) {
+    const results = new Map();
+    const keepTrackOfWords = [];
+
+    //takes each word from the words Array and adds each unique character in each word to Map key
+    words
+        .forEach((word)=>{
+            let charSet = new Set();
+            let alphaChar = word.toLowerCase().split("").sort().join("");
+            for (let char of alphaChar) {
+                charSet.add(char);
+            }
+            let uniqueChars = [...charSet]; //now uniqueChars.join("") returns the string in alphabetical order
+            if (results.has(uniqueChars.join(""))){
+                keepTrackOfWords.push(word.toLowerCase());
+                results.set(uniqueChars.join(""), word.toLowerCase());
+            } else {
+                results.set(uniqueChars.join(""), word.toLowerCase());
+            }
+        })
+        //if sorted string of letters is a key in the results Map then
+            //add word to the array associated with that key
+        //else
+            //make a new entry in the results Map with key = sorted string of letters
+    return results;
+}
+
+console.log(sameLetters(["pAir", "PER", "POlE", "peaR", "pEEr", "REap", "loPE"]));
+
+//--------------EXPECTED-------------//
+//  {   "aipr": ["pair"],
+//     "elop": ["pole", "lope"],
+//     "aepr": ["pear", "reap"],
+//     "epr": ["per", "peer"]
+//   }
 
 module.exports = sameLetters;
